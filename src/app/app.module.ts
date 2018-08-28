b2cc0e7437404b7f849a1b5e890208bf96311917
ctrl0703 <ctrl0703@ctrl0703s-MacBook-Pro.local>
2018-08-28 19:57:27 +0900

    add http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP } from '@ionic-native/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -17,7 +17,6 @@ import { ListPage } from '../pages/list/list';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -29,7 +28,7 @@ import { ListPage } from '../pages/list/list';
   providers: [
     StatusBar,
     SplashScreen,
-    HttpClientModule,
+    HTTP,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { SmartCard } from '../home/home';
 import { HomePage } from '../home/home';
+import { HTTP } from '@ionic-native/http';
 
 /**
  * Generated class for the ListPage page.
@@ -20,28 +20,49 @@ export class ListPage {
   //smartCards = new Array<SmartCard>();
   smartCards:string[] = ['일빵빵 영어회화1', '한자암기박사', '중국어회화'];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private http: HttpClient) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private http: HTTP) {
     let url="http://18.191.8.195:8080/smartcards";
-    //let headers = new HttpHeaders({'Content-Type': 'application/json'});
-    let headers = new HttpHeaders({
-      //'Content-Type': 'application/x-www-form-urlencoded',
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': 'http://localhost:8100',
-      'Access-Control-Allow-Methods': 'GET, POST, DELETE, PUT',
-      'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization, Origin, Accept'
-    });
-    let body={};
+    // let headers = new HttpHeaders({
+    //   'Access-Control-Allow-Origin': '*',
+    // });
 
-    this.http.post(url, body, {headers:headers}).subscribe((res:any)=>{
-      console.log("res:" + JSON.stringify(res));
-      let response = res;
-      if(response.result=="success") {
-        
-      } else {
-        
-      }
+    this.http.get(url, {}, {'Access-Control-Allow-Origin': '*'})
+    .then(data => {
+
+      console.log('data');
+      console.log(data.status);
+      console.log(data.data); // data received by server
+      console.log(data.headers);
+
+    })
+    .catch(error => {
+
+      console.log('err');
+      console.log(error.status);
+      console.log(error.error); // error message as string
+      console.log(error.headers);
+
     });
-    /*this.smartCards.push(new SmartCard());
+    //let headers = new HttpHeaders({'Content-Type': 'application/json'});
+  //  // let headers = new HttpHeaders({
+  //     //'Content-Type': 'application/x-www-form-urlencoded',
+  //     'Content-Type': 'application/json',
+  //     'Access-Control-Allow-Origin': 'http://localhost:8100',
+  //     'Access-Control-Allow-Methods': 'GET, POST, DELETE, PUT',
+  //     'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Authorization, Origin, Accept'
+  //   });
+  //   let body={};
+
+  //   this.http.post(url, body, {headers:headers}).subscribe((res:any)=>{
+  //     console.log("res:" + JSON.stringify(res));
+  //     let response = res;
+  //     if(response.result=="success") {
+        
+  //     } else {
+        
+  //     }
+  //   });
+     /*this.smartCards.push(new SmartCard());
     this.smartCards.push(new SmartCard());
     this.smartCards.push(new SmartCard());*/
   }
